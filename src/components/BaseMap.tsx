bce530627585c148d8cbc67bbafb4f0f390c42e0
Joseph Obeahon <jeobeahon@gmail.com>
2022-02-18 10:18:36 -0500

    my solution

--- src/components/BaseMap.tsx
+++ src/components/BaseMap.tsx
@@ -1,33 +1,101 @@
-import React, { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import MapView from "@arcgis/core/views/MapView";
 import ArcGISMap from "@arcgis/core/Map";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
-import TopFeaturesQuery from "@arcgis/core/rest/support/TopFeaturesQuery";
-type Props = {};
+import Graphic from "@arcgis/core/Graphic";
+import Point from "@arcgis/core/geometry/Point";
+import axios from "axios";
 
-function BaseMap({}: Props) {
+type NOAAFeature = {
+	geometry: {
+		type: string;
+		coordinates: [number, number];
+	};
+	properties: {
+		OBJECTID: number;
+		CZ_NAME: string;
+		EVENT_TYPE: string;
+		STATE: string;
+		Year: number;
+	};
+};
+
+function BaseMap() {
 	const mapDiv = useRef(null);
 
+	var thisMap = new ArcGISMap({
+		basemap: "gray-vector"
+	});
+
+	// const [thisMap, setThisMap] = useState<ArcGISMap>(
+	// 	new ArcGISMap({
+	// 		basemap: "gray-vector"
+	// 	})
+	// );
+
 	useEffect(() => {
+		initMap();
+	}, [thisMap]);
+
+	const getData = async () => {
+		const url =
+			"https://services.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/NOAA_Storm_Events_Database_view/FeatureServer/0/query";
+
+		const params = {
+			f: "pgeojson",
+			where: "state = 'NORTH CAROLINA' and EVENT_TYPE = 'Winter Storm' and Year = 2021",
+			outfields: "objectid, state, EVENT_TYPE, Year, CZ_NAME"
+		};
+
+		await axios
+			.get(url, { params })
+			.then((res) => {
+				if (res.data.features) {
+					let graphics: Graphic[] = res.data.features.map((entry: NOAAFeature) => {
+						const { coordinates } = entry.geometry;
+						const { OBJECTID, CZ_NAME, EVENT_TYPE, STATE, Year } = entry.properties;
+
+						return new Graphic({
+							attributes: {
+								OBJECTID,
+								CZ_NAME,
+								EVENT_TYPE,
+								STATE,
+								Year
+							},
+							geometry: new Point({
+								x: coordinates[0],
+								y: coordinates[1]
+							})
+						});
+					});
+
+					thisMap.layers.add(
+						new FeatureLayer({
+							source: graphics,
+							objectIdField: "OBJECTID"
+						})
+					);
+				} else {
+					throw new Error("No Data Returned");
+				}
+			})
+			.catch((err) => console.error(err));
+	};
+
+	const initMap = () => {
 		if (mapDiv.current) {
-			const stormData = new FeatureLayer({
-				url: "https://services.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/NOAA_Storm_Events_Database_view/FeatureServer/0/query",
-				outFields: ["NAME"]
-			});
-
-			const agMap = new ArcGISMap({
-				basemap: "gray-vector"
-				// layers: [stormData]
-			});
-
 			const view = new MapView({
 				container: mapDiv.current,
-				map: agMap,
+				map: thisMap,
 				center: [-80.793457, 35.782169],
-				zoom: 5
+				zoom: 6
 			});
 		}
-	}, []);
+	};
+
+	getData();
+
 	return <div className="mapDiv" ref={mapDiv} />;
 }
 
